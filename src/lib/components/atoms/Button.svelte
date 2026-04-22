a4e166b85db147c36f72ed2059d6f4dbe1428998
KUROYUKIHIME333 <dhermansum@gmail.com>
2026-04-23 00:40:05 +0100

    Create button component

---
+++ src/lib/components/atoms/Button.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import '../../../app.css';
+	export let type: 'button' | 'submit' | 'reset' | null | undefined = 'button';
+	export let variant: 'primary' | 'secondary' | 'tertiary' | 'custom' = 'primary';
+</script>
+
+<button {type} class="btn {variant}">
+	<slot />
+</button>
+
+<style>
+	.btn {
+		padding: 10px 20px;
+        width: 150px;
+        height: 50px;
+		border-radius: 35px;
+        border: 2px solid var(--primary-color);
+		cursor: pointer;
+	}
+	.primary {
+		background: linear-gradient(
+			90deg,
+			var(--primary-color) 0%,
+			var(--secondary-color) 51%,
+			var(--tertiary-color) 100%
+		);
+		color: white;
+	}
+    
+</style>
