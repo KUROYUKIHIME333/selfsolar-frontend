2b50ed182822931ef45e1a77c47f931faf26ad6c
KUROYUKIHIME333 <dhermansum@gmail.com>
2026-04-23 11:12:18 +0100

    feat: button loading

--- src/lib/components/atoms/Button.svelte
+++ src/lib/components/atoms/Button.svelte
@@ -12,10 +12,22 @@
 		| 'danger'
 		| 'good'
 		| 'clear' = 'primary';
+	export let disabled: boolean = false;
+	export let loading: boolean = false;
+	export let label: string;
 </script>
 
-<button {type} class="btn {variant}">
-	<slot />
+<button {type} class="btn {variant}" {disabled} on:click>
+	{#if loading}
+		<span>
+			<span class="btn-spinner spinner-1"></span>
+			<span class="btn-spinner spinner-2"></span>
+			<span class="btn-spinner spinner-3"></span>
+		</span>
+	{:else}
+		{label}
+		<slot />
+	{/if}
 </button>
 
 <style>
@@ -75,5 +87,58 @@
 		color: var(--secondary-color);
 		width: fit-content;
 		height: fit-content;
+	}
+
+	.btn-spinner {
+		display: inline-block;
+		width: 10px;
+		height: 10px;
+		background: white;
+		border-radius: 50%;
+	}
+
+	.spinner-1 {
+		animation: pulse-1 1s infinite;
+	}
+	.spinner-2 {
+		animation: pulse-2 1s infinite;
+	}
+	.spinner-3 {
+		animation: pulse-3 1s infinite;
+	}
+
+	@keyframes pulse-2 {
+		0%,
+		100% {
+			transform: scale(0.5);
+			opacity: 1;
+		}
+		50% {
+			transform: scale(1);
+			opacity: 1;
+		}
+	}
+	@keyframes pulse-3 {
+		0%,
+		50% {
+			transform: scale(0.5);
+			opacity: 1;
+		}
+		100% {
+			transform: scale(1);
+			opacity: 1;
+		}
+	}
+
+	@keyframes pulse-1 {
+		0% {
+			transform: scale(1);
+			opacity: 1;
+		}
+		50%,
+		100% {
+			transform: scale(0.5);
+			opacity: 1;
+		}
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
 	import Button from '../lib/components/atoms/Button.svelte';
 	import Header from '../lib/components/molecules/Header.svelte';
 
+	let loading = true;
 </script>
 
 <svelte:head>
@@ -13,7 +14,7 @@
 </svelte:head>
 
 <section>
-<Header></Header>
+	<Header></Header>
 	<!-- <h1>
 		<span class="welcome">
 			<picture>
@@ -29,7 +30,7 @@
 		try editing <strong>src/routes/+page.svelte</strong>
 	</h2> -->
 
-	<Button variant="primary"> Clique</Button>
+	<Button variant="primary" label="Click" {loading}>ici</Button>
 </section>
 
 <style>
@@ -39,25 +40,5 @@
 		justify-content: center;
 		align-items: center;
 		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
 	}
 </style>
