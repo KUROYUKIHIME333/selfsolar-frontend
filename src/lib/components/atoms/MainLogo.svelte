abb32b957ca14ef8fba81ebb89bd7ee995d6464a
KUROYUKIHIME333 <dhermansum@gmail.com>
2026-04-24 12:53:46 +0100

    update logo color: a stylish gradient

--- src/lib/components/atoms/MainLogo.svelte
+++ src/lib/components/atoms/MainLogo.svelte
@@ -9,8 +9,18 @@
 
 <style>
 	span {
-		font-size: larger;
-		font-weight: 700;
+		background: linear-gradient(
+			90deg,
+			var(--primary-color) 0%,
+			var(--secondary-color) 51%,
+			var(--tertiary-color) 100%
+		);
+		background-clip: text;
+		-webkit-background-clip: text;
+		-webkit-text-fill-color: transparent;
+
+		font-size: var(--big-title-size);
+		font-weight: var(--big-title-weight);
 		color: var(--primary-color);
 	}
 </style>
