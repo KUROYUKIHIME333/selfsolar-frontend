3dc0e3e0eafc2941e03bc6b2f884bce95f87ae25
KUROYUKIHIME333 <dhermansum@gmail.com>
2026-04-27 08:57:01 +0100

    The idea is that I use this component in all section by givinng it props and slots

---
+++ src/lib/components/atoms/PVFormSectionContent.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	let {
+		visibility = true,
+		sectionTitle,
+		sectionDescription
+	}: {
+		visibility?: boolean;
+		sectionTitle: string;
+		sectionDescription: HTMLElement;
+	} = $props();
+</script>
+
+<fieldset class={`form-section ${visibility ? '' : 'is-hidden-now'}`}>
+	<legend class="section-legend">
+		<h2>{sectionTitle}</h2>
+	</legend>
+
+	<div class="section-content">
+		<p class="section-description">
+			{sectionDescription}
+		</p>
+		<slot />
+	</div>
+</fieldset>
+
+<style></style>
