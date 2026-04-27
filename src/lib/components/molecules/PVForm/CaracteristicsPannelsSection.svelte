cacba8f93dac17460b61f3e5f843f2fe06ff8892
KUROYUKIHIME333 <dhermansum@gmail.com>
2026-04-27 08:58:14 +0100

    Workin on it

---
+++ src/lib/components/molecules/PVForm/CaracteristicsPannelsSection.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import Select from '$components/atoms/Select.svelte';
+	import Input from '$components/atoms/Input.svelte';
+	let pannelsDatas = $props();
+	let {
+		visibility = true,
+		puissanceCreteModule,
+		tensionVoc,
+		courantCourtCircuit,
+		tensionMPP,
+		courantMPP,
+		coeffTempTension,
+		coeffTempPuissance,
+		noct
+	}: {
+		visibility?: boolean;
+		puissanceCreteModule: Tnumber;
+		tensionVoc: number;
+		courantCourtCircuit: number;
+		tensionMPP: number;
+		courantMPP?: number;
+		coeffTempTension: number;
+		coeffTempPuissance: number;
+		noct?: number;
+	} = $props();
+
+	let {
+		temperatureMin,
+		temperatureMax,
+		irradianceMax
+	}: {
+		temperatureMin: number;
+		temperatureMax: number;
+		irradianceMax: number;
+	} = $props();
+</script>
+
+<fieldset class={`form-section ${visibility ? '' : 'is-hidden-now'}`}>
+	<legend class="section-legend">
+		<h2>Caractéristique des panneaux</h2>
+	</legend>
+
+	<div class="section-content">
+		<p class="section-description">
+			<span
+				>Avoir les caractéristique du modèle de panneaux permettra de faire une installation propre
+				et efficace, adapté à vos besoins.</span
+			>
+			<span
+				>Le fonctionnement de ces derniers, et donc de votre installation, sera aussi affecté par la
+				météo, en particuliers la température ambiante.</span
+			>
+		</p>
+
+		<div class="section-datas-style">
+			<p>
+				Choisir un panneau dans la liste proposés. Ou bien, renseigner les infos soi-même avec
+				l'option <span>custom</span>
+			</p>
+			<Select
+				name="typeInstallation"
+				label="A quoi s'attendre ? Qu'est ce qui décrit le mieux l'installation et son milieu ?"
+				value={installation}
+				options={TYPE_INSTALATTION_OPTIONS}
+				isRequired={true}
+			/>
+		</div>
+	</div>
+</fieldset>
+
+<style></style>
